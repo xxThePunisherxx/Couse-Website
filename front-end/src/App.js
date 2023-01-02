50fd069bc2ce0eb2efaead292e807fcf942d2fc0
Nikesh <nikeshkhatri763@gmail.com>
2023-01-03 01:44:07 +0545

    HomePage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navigation from "./Components/Navigation/Navigation";
+import Homepage from "./Pages/Homepage/Homepage";
+import Footer from "./Components/Footer/Footer";
+import Courses from "./Pages/Courses/Courses";
+import Comming from "./Pages/CommingSoon/Comming";
+// import Admin from "./Pages/Admin/Admin";
 
 function App() {
 	return (
-		<div className="App">
-			<h1>this is test</h1>
-		</div>
+		<>
+			<BrowserRouter>
+				<Navigation />
+				<Routes>
+					<Route path="/" element={<Homepage />} />
+					<Route path="/courses" element={<Courses />} />
+					<Route path="/team" element={<Comming />} />
+					<Route path="/aboutUS" element={<Comming />} />
+					<Route path="contact" element={<Comming />} />
+					<Route path="/gallery" element={<Comming />} />
+					<Route path="/verify" element={<Comming />} />
+					<Route path="/vacancy" element={<Comming />} />
+					<Route path="/services" element={<Comming />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</>
 	);
 }
 
