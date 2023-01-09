4c2975088cd4157c715e920dc3b754c9d5805319
Nikesh <nikeshkhatri763@gmail.com>
2023-01-10 02:19:02 +0545

    Close navBar when clicked outside of menu& route change

--- front-end/src/Components/Navigation/Navigation.jsx
+++ front-end/src/Components/Navigation/Navigation.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./Navigation.module.css";
 import { NavLink } from "react-router-dom";
 import { Links } from "../../Data/Navlink";
 
 const Navigation = () => {
+	const [Navtogled, setNavtogled] = useState(false);
+	useEffect(() => {
+		let handler = (event) => {
+			if (!navRef.current.contains(event.target)) {
+				setNavtogled(false);
+				console.log(Navtogled);
+			}
+		};
+		document.addEventListener("mousedown", handler);
+		return () => {
+			document.removeEventListener("mousedown", handler);
+		};
+	});
+
+	let navRef = useRef();
 	return (
 		<div>
-			<header className={style.header}>
+			<header className={style.header} ref={navRef}>
 				<div className={style.Navigation_Container}>
 					<div className={style.Links}>
 						<a href="/" className={style.logo}>
 							<div className={style.headerrlogo}></div>
 						</a>
-						<input type="checkbox" id={style.navCheck}></input>
+						<input type="checkbox" id={style.navCheck} onClick={() => setNavtogled((Navtogled) => !Navtogled)} checked={Navtogled} readOnly></input>
 						<label htmlFor={style.navCheck} className={style.navToggler}>
 							<span></span>
 						</label>
