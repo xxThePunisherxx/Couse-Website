d3add7c2abf91ccae3df60f4a49b7e5eda4c09b7
Nikesh <nikeshkhatri763@gmail.com>
2023-02-10 00:56:55 +0545

    Display admin list in dashboard

--- front-end/src/Components/AdminList/AdminList.jsx
+++ front-end/src/Components/AdminList/AdminList.jsx
@@ -1,31 +1,55 @@
 import React from "react";
 import style from "./AdminList.module.css";
-import { AdminListData } from "../../Data/AdminList";
 import { MdModeEditOutline, MdDeleteSweep } from "react-icons/md";
+import useFetchAuth from "../../Utils/Hooks/fetchAuth";
+
+import uuid from "react-uuid";
 
 const AdminList = () => {
+	const dummyArr = [0, 1, 2]; // just for adding skeleton.
+
+	const { data: trainingData, ispending } = useFetchAuth("http://localhost:8080/api/user/admin/users");
+	console.log(trainingData.users);
 	return (
-		<div>
-			<div className={style.allAdminWrapper}>
-				<h1>Admin list</h1>
-				<div className={style.allAdminGrid}>
-					{AdminListData.map((Admin) => (
-						<div key={Admin.image} className={style.admin_Table}>
-							<img src={Admin.image} alt={Admin.Name}></img>
-							<h1>{Admin.Name}</h1>
-							<h1>{Admin.Role}</h1>
-							<h1>{Admin.contact}</h1>
-							<button className={style.Edit_Btn}>
-								<MdModeEditOutline />
-							</button>
-							<button className={style.Delete_Btn}>
-								<MdDeleteSweep />
-							</button>
+		<div className={style.AdminList}>
+			<h1>
+				Admin <span className={style.HeadingHighlight}>List</span>
+			</h1>
+			{ispending && (
+				<div className={style.TrainingGrid}>
+					{dummyArr.map(() => (
+						<div key={uuid()} className={style.Training}>
+							<div className={style.Skel}>
+								<div className={style.H1Div}></div>
+								<div className={style.H1Div}></div>
+								<div className={style.H1Div}></div>
+								<div className={style.H1Div}></div>
+							</div>
 						</div>
 					))}
-					<button className={style.new}>Add admin</button>
 				</div>
-			</div>
+			)}
+			{!ispending && (
+				<div className={style.allAdminWrapper}>
+					<div className={style.allAdminGrid}>
+						{trainingData.users.map((Admin) => (
+							<div key={uuid()} className={style.admin_Table}>
+								<h1>{Admin.name}</h1>
+								<h1>{Admin.role}</h1>
+								<h1>{Admin.email}</h1>
+
+								<button className={style.Edit_Btn}>
+									<MdModeEditOutline />
+								</button>
+								<button className={style.Delete_Btn}>
+									<MdDeleteSweep />
+								</button>
+							</div>
+						))}
+						{!ispending && <button className={style.new}>Add admin</button>}
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
