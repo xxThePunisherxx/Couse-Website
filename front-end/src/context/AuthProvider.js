1ac92c95b0f2ee01610c5ab3674da4b980bdd4c6
Nikesh <nikeshkhatri763@gmail.com>
2023-02-14 23:54:05 +0545

    Delete contents of localstorage when token expires

--- front-end/src/context/AuthProvider.js
+++ front-end/src/context/AuthProvider.js
@@ -3,13 +3,28 @@ import { createContext, useState } from "react";
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
+	const d = new Date();
+	const LoginTime = d.getTime();
 	let parsedLocalData = { Role: "", Token: "" };
+
 	const localStoredData = localStorage.getItem("User Info");
+	const FirstLoginTime = JSON.parse(localStorage.getItem("User time"));
+	let Expire = false;
 	if (localStoredData) {
+		Expire = LoginTime - FirstLoginTime.StoreLoginTime >= 86400000;
+	}
+	if (Expire) {
+		localStorage.clear();
+	}
+
+	if (localStoredData && !Expire) {
 		parsedLocalData = JSON.parse(localStoredData);
 	}
-	const [auth, setAuth] = useState({ Role: parsedLocalData.LocalRole, Token: parsedLocalData.LocalToken });
-	// const [auth, setAuth] = useState({});
+
+	const [auth, setAuth] = useState({
+		Role: parsedLocalData.LocalRole,
+		Token: parsedLocalData.LocalToken,
+	});
 
 	return <AuthContext.Provider value={{ auth, setAuth }}>{children}</AuthContext.Provider>;
 };
