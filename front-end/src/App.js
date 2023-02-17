fbb365b99127409154696cd54f73d9b7df680235
Nikesh <nikeshkhatri763@gmail.com>
2023-02-17 21:56:34 +0545

    Added Google Form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,20 +7,16 @@ import ClientsAll from "./Pages/Clients/ClientsAll";
 import NotFound from "./Pages/NotFound/NotFound";
 import Admin from "./Pages/Admin/Admin";
 import Test from "./Pages/TestPage/Test";
-// import ScrollToTop from "./Components/ScrollToTop";
 import IndividualCourse from "./Pages/IndividualCourse/IndividualCourse";
 import ClientLayout from "./Components/ClientLayout/ClientLayout";
 import AddCourse from "./Pages/Add courses/AddCourse";
 import AdminLayout from "./Components/AdminLayout/AdminLayout";
-// import RemoveCourse from "./Pages/Update course/UpdateCourse";
 import AdminDashboard from "./Pages/AdminDashboard/AdminDashboard";
-// import AdminSidebar from "./Components/Admin Side Bar/AdminSidebar";
-import EnrollForm from "./Components/Enroll Form/EnrollForm";
+// import EnrollForm from "./Components/Enroll Form/EnrollForm";
 import Updatecourse from "./Pages/Update course/UpdateCourse";
 import AddCourseCate from "./Pages/Add Course Category/AddCourseCate";
 import UpdateCourseCategory from "./Pages/Update course Category/UpdateCourseCategory";
 import AdminCourseListPage from "./Pages/Admin Course Page/AdminCourseListPage";
-
 import RequireAuth from "./Components/RequireAuth";
 import Addadmin from "./Pages/Add admin/Addadmin";
 import Team from "./Pages/Our Team/Team";
@@ -28,6 +24,9 @@ import Contact from "./Pages/Contact us/Contact";
 import Unauthorized from "./Pages/Unauthorized/Unauthorized";
 import UpdateAdmin from "./Pages/Update Admin/UpdateAdmin";
 import AddTestomonial from "./Pages/Add Testomonial/AddTestomonial";
+import AboutUs from "./Pages/About Us/AboutUs";
+import GoogleForm from "./Pages/Enquirery/GoogleForm";
+import EnrollForm from "./Pages/Enroll/EnrollForm";
 
 function App() {
 	return (
@@ -38,14 +37,16 @@ function App() {
 				<Route path="/courses" element={<Courses />} />
 				<Route path="/enroll" element={<EnrollForm />} />
 				<Route path="/team" element={<Team />} />
-				<Route path="/aboutUS" element={<Comming />} />
+				<Route path="/aboutUS" element={<AboutUs />} />
 				<Route path="contact" element={<Contact />} />
 				<Route path="/gallery" element={<Comming />} />
-				<Route path="/services" element={<Comming />} />
+				<Route path="/studentProjects" element={<Comming />} />
 				<Route path="/allClients" element={<ClientsAll />} />
 				<Route path="/unauthorized" element={<Unauthorized />} />
 				<Route path="/test" element={<Test />} /> just for quick testing of page
 				<Route path="/course-view/:courseID" element={<IndividualCourse />} />
+				<Route path="/inquiry/googleForm" element={<GoogleForm />} />
+				<Route path="/enroll/googleForm" element={<EnrollForm />} />
 				<Route path="*" element={<NotFound />} />
 			</Route>
 			<Route>
